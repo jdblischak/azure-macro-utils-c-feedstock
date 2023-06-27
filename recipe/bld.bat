53904c0322979fcd28cc1bed05284345222ea844
John Blischak <jdblischak@gmail.com>
2023-06-27 15:25:56 -0400

    Debug install configuration setting

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,11 +3,15 @@
 mkdir build
 cd build
 
+echo %CMAKE_ARGS%
+
 cmake ^
     -G "Ninja" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     ..
 if errorlevel 1 exit 1
+
+type CMakeCache.txt
 
 :: Install.
 cmake --build . --config Release --target install
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+echo "CMAKE_ARGS=$CMAKE_ARGS"
+
 mkdir -p build
 cd build
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     ..
+
+cat CMakeCache.txt
 
 cmake --build . --config Release --target install
