14fef4120080ef837aa3c8192d79d0ca594d3400
John Blischak <jdblischak@gmail.com>
2023-06-27 14:41:51 -0400

    Use fewer CMake args

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,13 +8,9 @@ cd build
 cmake ^
     -G "Ninja" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True ^
-    -DBUILD_SHARED_LIBS=ON ^
-    -Duse_installed_dependencies=ON ^
-    %SRC_DIR%
+    ..
 if errorlevel 1 exit 1
 
 :: Install.
-cmake --build . --config Release --target install
+cmake --build . --target install
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -6,11 +6,6 @@ cd build
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
-    -DBUILD_SHARED_LIBS=ON \
-    -Duse_installed_dependencies=ON \
-    ${SRC_DIR}
+    ..
 
-make -j$CPU_COUNT
-make install
+cmake --build . --target install
